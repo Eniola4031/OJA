0b346d29f2470c4a39b1b751703806f4c8418574
eniola4031 <agboola.eniola4031@gmail.com>
2021-11-02 11:13:50 +0100

    added links

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,21 @@ function ojaMarketplace({Component, pageProps}){
   return(
     <div>
       <nav className='border-b p-6' style={{backgroundColor:'white'}}>
-        <p className= 'text-4x1 font-bold text-black'> OJA AFRICAN NFT MARKETPLACE
+        <p className='text-4x1 font-bold text-black'> OJA AFRICAN NFT MARKETPLACE
         </p>
-        <div>
+        <div className='flex mt-4 justify-center'>
+          <Link href='/'>
+            <a className='mr-4'> Main MarketPlace</a>
+          </Link>
+          <Link href='/'>
+            <a className='mr-4'> Main MarketPlace</a>
+          </Link>
+           <Link href='/'>
+            <a className='mr-4'> Main MarketPlace</a>
+          </Link> 
+          <Link href='/'>
+            <a className='mr-4'> Main MarketPlace</a>
+          </Link>
         </div>
       </nav>
     </div>
