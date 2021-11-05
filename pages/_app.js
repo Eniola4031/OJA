77f27f5b7b777560f6a7d8c9df545823326d4bc8
eniola4031 <agboola.eniola4031@gmail.com>
2021-11-05 11:31:36 +0100

    added styling

--- pages/_app.js
+++ pages/_app.js
@@ -4,25 +4,28 @@ import Link from 'next/link'
 function ojaMarketplace({Component, pageProps}){
   return(
     <div>
-      <nav className='border-b p-6' style={{backgroundColor:'white'}}>
+      <nav className='border-b p-6' style={{backgroundColor:'skyblue'}}>
         <p className='text-4x1 font-bold text-black'> OJA AFRICAN NFT MARKETPLACE
         </p>
         <div className='flex mt-4 justify-center'>
           <Link href='/'>
-            <a className='mr-4'> Main MarketPlace</a>
+            <a className='mr-6'> Home</a>
           </Link>
-          <Link href='/'>
-            <a className='mr-4'> Main MarketPlace</a>
+          <Link href='mint-item'>
+            <a className='mr-6'> Mint Item</a>
           </Link>
-           <Link href='/'>
-            <a className='mr-4'> Main MarketPlace</a>
+           <Link href='/my-nfts'>
+            <a className='mr-6'>My NFTs</a>
           </Link> 
-          <Link href='/'>
-            <a className='mr-4'> Main MarketPlace</a>
+          <Link href='/account-dashbaord'>
+            <a className='mr-6'> Account Dashboard</a>
           </Link>
         </div>
       </nav>
+      <Component {...pageProps} />
     </div>
     
   )
 }
+
+export default ojaMarketplace
