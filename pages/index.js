fcb3b2d7abcd368545f85199bf523a5f7e9b1b16
eniola4031 <agboola.eniola4031@gmail.com>
2021-11-09 01:09:32 +0100

    added index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-
+import {ethers} from 'ethers'
+import {useEffect, useState} from 'react'
+import axios from 'axios'
+import web3Modal from 'web3Modal'
 
 export default function Home() {
   return (
     <div>
-     OJA NFT MARKETPLACE
     </div>
   )
 }
