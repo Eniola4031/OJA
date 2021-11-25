d82bb19cd31d005f9cff161212cca768016ae5cb
eniola4031 <agboola.eniola4031@gmail.com>
2021-11-25 11:29:53 +0100

    fixed errors

--- pages/mint-item.js
+++ pages/mint-item.js
@@ -5,16 +5,17 @@ import {create as ipfsHttpClient} from 'ipfs-http-client'
 import{nftAddress, nftMarketAddress} from '../config'
 // in this component, we set the ipfs up to host out nft data of file storage
 import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
+//import {useRouter} from 'next/router'
 import {useRouter} from 'next/router'
 import Oja from '../artifacts/contracts/oja.sol/oja.json'
 
-const client= ipfsHttpClient('https://ipfs.infura.io:5001/api/v0',)
+const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0',)
 
 export  default function mintItem(){
     const[fileUrl, setFileUrl]= useState(null)
     const [formInput, updateFormInput] = useState({price:'', name:'', description:''})
 
-    const router = userRouter()
+    const router = useRouter()
 
     //set up a funtion to fireoff when we update files in our form - we can add our NFT images- IPFS
      async function onChange(e){
@@ -35,7 +36,7 @@ export  default function mintItem(){
      }
 
 async function createMarket(){
-    const {name, price, description} = formInput
+    const {name, description, price} = formInput
     if(!name || !description || !price || !fileUrl) return
     //upload to IPFS
     const data = JSON.stringify({
@@ -71,7 +72,7 @@ async function createSale(url){
     let listingPrice = await contract.getListingPrice()
     listingPrice = listingPrice.toString()
 
-    transaction = await contract.makeMarketItem(nftAddress, tokenId, price, {value: lisitingPrice})
+    transaction = await contract.makeMarketItem(nftaddress, tokenId, price, {value: listingPrice})
     await transaction.wait()
     router.push('./')
 }
@@ -80,10 +81,16 @@ return (
     <div className='flex justify-center'>
         <div className='w-1/2 flex flex-col pb-12'>
             <input placeholder='Asset Name' className='mt-8 border rounded p-4' onChange={ e => updateFormInput({...formInput, name: e.target.value})}/>
+
             <textarea placeholder='Asset description' className='mt-8 border rounded p-4' onChange={ e => updateFormInput({...formInput, description: e.target.value})}/>
-            <input type='file' name='Asset' className='mt-4' onChange={ e => updateFormInput({...formInput, price: e.target.value})}/>
+
+            <input placeholder='Asset Price in ETH' className='mt-2 border rounded p-4' onChange={ e => updateFormInput({...formInput, price: e.target.value})} />
+
+            <input type='file' name='Asset' className='mt-4' onChange={onChange}/>
             {fileUrl && (
-                <img className='rounded mt-4' width='350px' src={fileUrl}/>  )}
+                <img className='rounded mt-4' width='350px' src={fileUrl}/>  
+                )
+            }
                 <button onClick={createMarket} className='font-bold mt-4 bg-purple-500 text-white rounded p-4 shadow-lg'> Mint NFT</button>
         </div>
     </div>
